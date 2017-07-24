0fd08244190b6c1c6ec73e78639d918856dccfef
Sean Felling <Snfelling7@gmail.com>
2017-07-24 12:52:31 -0500

    commented out post/get/info on app.js

--- server/app.js
+++ server/app.js
@@ -7,22 +7,22 @@ var path = require('path');
 app.use(express.static(path.resolve('./server/public')));
 
 // server index file
-app.get('/info', function(req, res) {
-    res.send("hello from the server");
-});
+// app.get('/info', function(req, res) {
+//     res.send("hello from the server");
+// });
 
 app.use('/', function(req, res) {
   res.sendFile(path.join(__dirname, './public/views/index.html'));
 });
 
-app.get( '/', function( req, res )
-{
-
-});
-
-app.post( '/', function( req, res ) {
-
-});
+// app.get( '/', function( req, res )
+// {
+//
+// });
+//
+// app.post( '/', function( req, res ) {
+//
+// });
 
 app.listen(3007, function() {
   console.log("server running, check localhost:3007");
