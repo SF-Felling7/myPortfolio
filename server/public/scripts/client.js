f5a5682eb881901c69369cb360fc73a88d7bf3b8
Sean Felling <Snfelling7@gmail.com>
2017-07-24 13:08:29 -0500

    Added angular config

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,20 @@
 console.log( 'Working' );
+
+var portFolioApp = angular.module( 'portFolioApp', ['ng-route'] );
+
+
+portFolioApp.config(function($routeProvider, $locationProvider) {
+ $routeProvider.when('/about', {
+   templateUrl: '/views/pages/about.html',
+   controller: 'aboutController as ac'
+ }).when( '/contact',{
+   templateUrl: '/views/pages/contact.html',
+   controller: 'contactController as cc'
+ }).when( '/gallery', {
+   templateUrl: '/views/pages/gallery.html',
+   controller: 'galleryController as gc'
+ }).otherwise('/');
+
+ $locationProvider.html5Mode(true);
+
+});
