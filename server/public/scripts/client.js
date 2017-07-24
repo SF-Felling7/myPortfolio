e2c3720375628c94c9483a30f2902333bfc55656
Sean Felling <Snfelling7@gmail.com>
2017-07-24 17:32:31 -0500

    Edited the angular config and added another view called home.html

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,8 +4,10 @@ var portFolioApp = angular.module( 'portFolioApp', ['ngRoute'] );
 
 
 portFolioApp.config(function($routeProvider, $locationProvider) {
- $routeProvider.when('/about', {
-   templateUrl: '/views/pages/about.html',
+ $routeProvider.when('/home', {
+   templateUrl: '/views/pages/home.html'
+ }).when( '/about', {
+   templateUrl: '/views/pages/about.html'
  }).when( '/contact',{
    templateUrl: '/views/pages/contact.html'
  }).when( '/gallery', {
