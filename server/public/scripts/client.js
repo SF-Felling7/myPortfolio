05bbd8fd98553d504b2322179e6c4e6ae5d8b4c7
Sean Felling <Snfelling7@gmail.com>
2017-07-26 01:03:09 -0500

    commented out code that is yet to work

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,22 +1,22 @@
-console.log( 'Working' );
+console.log('Working');
 
-var portFolioApp = angular.module( 'portFolioApp', ['ngRoute'] );
+var portFolioApp = angular.module('portFolioApp', ['ngRoute']);
 
 
 portFolioApp.config(function($routeProvider, $locationProvider) {
- $routeProvider.when('/', {
+  $routeProvider.when('/', {
     template: '<div></div>',
     controller: ''
- }).when('/home', {
-   templateUrl: '/views/pages/home.html'
- }).when( '/about', {
-   templateUrl: '/views/pages/about.html'
- }).when( '/contact',{
-   templateUrl: '/views/pages/contact.html'
- }).when( '/gallery', {
-   templateUrl: '/views/pages/gallery.html'
- }).otherwise('/');
+  }).when('/home', {
+    templateUrl: '/views/pages/home.html'
+  }).when('/about', {
+    templateUrl: '/views/pages/about.html'
+  }).when('/contact', {
+    templateUrl: '/views/pages/contact.html'
+  }).when('/gallery', {
+    templateUrl: '/views/pages/gallery.html'
+  }).otherwise('/');
 
- $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(true);
 
 });
