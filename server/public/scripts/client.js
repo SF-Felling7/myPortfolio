d75ce03ea335722efc72cef07832a151664208a2
Sean Felling <Snfelling7@gmail.com>
2017-07-25 22:02:22 -0500

    edited client.js and took practice lines of code that were not working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,7 +4,10 @@ var portFolioApp = angular.module( 'portFolioApp', ['ngRoute'] );
 
 
 portFolioApp.config(function($routeProvider, $locationProvider) {
- $routeProvider.when('/home', {
+ $routeProvider.when('/', {
+    template: '<div></div>',
+    controller: ''
+ }).when('/home', {
    templateUrl: '/views/pages/home.html'
  }).when( '/about', {
    templateUrl: '/views/pages/about.html'
