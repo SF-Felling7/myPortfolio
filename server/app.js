82d6122c69915d8b40d5ed319e9717ebaf02ce54
Sean Felling <Snfelling7@gmail.com>
2017-07-29 11:14:32 -0500

    took out commented code that wasn't being used

--- server/app.js
+++ server/app.js
@@ -6,10 +6,6 @@ var path = require('path');
 // serve static files
 app.use(express.static(path.resolve('./server/public')));
 
-// server index file
-// app.get('/info', function(req, res) {
-//     res.send("hello from the server");
-// });
 
 app.use('/', function(req, res) {
   res.sendFile(path.join(__dirname, './public/views/index.html'));
