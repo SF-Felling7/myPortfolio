a08b72cfc4594e03a8aa1ea7e5cb0bee05dd5a4b
Sean Felling <Snfelling7@gmail.com>
2017-08-05 18:19:52 -0500

    loads automatically to the home page

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,7 +3,7 @@ var portFolioApp = angular.module('portFolioApp', ['ngRoute']);
 
 portFolioApp.config(function($routeProvider, $locationProvider) {
   $routeProvider.when('/', {
-    template: '<div></div>',
+    template: '<div><header id="main"><h1 class="myBrand">Sean F. Felling</h1><h3 class="myBrand">Full Stack Web Developer</h3></header></div>',
     controller: ''
   }).when('/home', {
     templateUrl: '/views/pages/home.html'
